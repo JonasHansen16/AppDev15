61a2e3f83491524c40bc3cd11b21dc01c6d51459
stefanie-r <Stefanie.Ramaekers@student.pxl.be>
2015-05-11 14:12:53 +0200

    aanpassen gui en beetje code admin gui + aanmaken class hulpverlener

---
+++ finah-desktop/desktopClient/desktopClient/Hulpverlener.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint_1_def
+{
+    class Hulpverlener
+    {
+        private string naam;
+
+        public string Naam
+        {
+            get { return naam; }
+            set { naam = value; }
+        }
+        private string voornaam;
+
+        public string Voornaam
+        {
+            get { return voornaam; }
+            set { voornaam = value; }
+        }
+        private string username;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        private string beroep;
+
+        public string Beroep
+        {
+            get { return beroep; }
+            set { beroep = value; }
+        }
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public Hulpverlener(string name)
+        {
+            this.Naam = name;
+        }
+    }
+}
